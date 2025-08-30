841088d9ef97a2b3fef68eeb63f0d8f86b179d60
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-31 01:57:49 +0300

    hello world with graphql

--- index.ts
+++ index.ts
@@ -2,21 +2,62 @@ import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./configs/database"
 import { Article } from "./models/article.model"
-
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from '@apollo/server/standalone';
 dotenv.config()
 
-const app: Express = express()
-const port: number = 3000
+const startSever = async () => {
+  const app: Express = express()
+  const port: number = 3000
 
-connectDB()
+  connectDB()
 
-app.get("/articles", async (req: Request, res: Response) => {
-  const articles = await Article.find({
-    deleted: false
+  // REST API ==================================================================
+
+  // app.get("/articles", async (req: Request, res: Response) => {
+  //   const articles = await Article.find({
+  //     deleted: false
+  //   })
+  //   res.json({
+  //     articles: articles
+  //   })
+  // })
+
+  // END REST API ==================================================================
+
+  // GraphQL API ==================================================================
+  const typeDefs = `#graphql
+    type Query {
+      hello: String
+    }
+  `
+
+  const resolvers = {
+    Query: {
+      hello: () => {
+        return "Hello world!"
+      }
+    }
+  }
+
+  const apolloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
   })
-  res.json(articles)
-})
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  // startStandaloneServer sẽ tự động tạo Express app riêng
+  const { url } = await startStandaloneServer(apolloServer, {
+    listen: { port: port },
+  });
+
+  console.log(`🚀 GraphQL Server ready at: ${url}`);
+
+
+  // END GraphQL API ==================================================================
+
+  // app.listen(port, () => {
+  //   console.log(`App listening on port ${port}`)
+  // })
+}
+
+startSever()
